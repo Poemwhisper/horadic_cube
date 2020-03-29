8394f137635a1de4d4eeb7680cd08c1a01059381
Nada404 <poemwhisper@sina.com>
2020-03-29 22:53:28 +0900

    pygame practice 3/29

--- horadic_cube.py
+++ horadic_cube.py
@@ -5,25 +5,85 @@ win = pygame.display.set_mode((500,500))
 
 pygame.display.set_caption("horadic cube")
 
-x = 50
-y = 50
-width = 40
-height = 60
-vel =5
+walkRight = [pygame.image.load('Game/R1.png'), pygame.image.load('Game/R2.png'), pygame.image.load('Game/R3.png'), pygame.image.load('Game/R4.png'), pygame.image.load('Game/R5.png'), pygame.image.load('Game/R6.png'), pygame.image.load('Game/R7.png'), pygame.image.load('Game/R8.png'), pygame.image.load('Game/R9.png')]
+walkLeft = [pygame.image.load('Game/L1.png'), pygame.image.load('Game/L2.png'), pygame.image.load('Game/L3.png'), pygame.image.load('Game/L4.png'), pygame.image.load('Game/L5.png'), pygame.image.load('Game/L6.png'), pygame.image.load('Game/L7.png'), pygame.image.load('Game/L8.png'), pygame.image.load('Game/L9.png')]
+bg = pygame.image.load('Game/bg.jpg')
+char = pygame.image.load('Game/standing.png')
 
+clock = pygame.time.Clock()
+
+class player(object):
+    def __init__(self, x, y, width, height):
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.vel = 5
+        self.isJump = False
+        self.jumpCount = 10
+        self.left = False
+        self.right = False
+        self.walkCount = 0
+
+    def draw(self,win):
+        if self.walkCount + 1 >= 27:
+            self.walkCount = 0
+
+        if self.left:
+            win.blit(walkLeft[self.walkCount//3],(self.x,self.y))
+            self.walkCount += 1
+        elif self.right:
+            win.blit(walkRight[self.walkCount//3],(self.x,self.y))
+            self.walkCount += 1
+        else:
+            win.blit(char, (self.x,self.y))
+
+def redrawGameWindow():
+    win.blit(bg,(0,0))
+    man.draw(win)
+    pygame.display.update()
+
+#mainloop
+man = player(300,410,64,64)
 run = True
 while run:
-    pygame.time.delay(100)
+    clock.tick(27) #fps 27
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
     keys = pygame.key.get_pressed()
+    
+    if keys[pygame.K_LEFT] and man.x > man.vel:
+        man.x -= man.vel
+        man.left = True
+        man.right = False
+    elif keys[pygame.K_RIGHT] and man.x < 500 - man.width - man.vel:
+        man.x += man.vel
+        man.right = True
+        man.left = False
+    else:
+        man.right = False
+        man.left = False
+        man.walkCount = 0
+    
+    if not (man.isJump):
+        if keys[pygame.K_SPACE]:
+            man.isJump = True
+            man.right = False
+            man.left = False
+            man.walkCount = 0
 
-    if keys[]
+    else:
+        if man.jumpCount >= -10:
+            man.y -= (man.jumpCount *abs(man.jumpCount)) * 0.5
+            print(man.y)
+            man.jumpCount -= 1
+        else:
+            man.isJump = False
+            man.jumpCount = 10
 
-    pygame.draw.rect(win,(255,0,0),(x,y,width,height))
-    pygame.display.update()
+    redrawGameWindow()
 
 pygame.quit()
---
+++ test.py
@@ -0,0 +1,13 @@
+token = True
+i = 1
+
+if not(token):
+    print ('hello')
+
+else:
+    if i < 10:
+        i+=1
+        print(i) 
+    else:
+        print('ok')
+        print(i)
